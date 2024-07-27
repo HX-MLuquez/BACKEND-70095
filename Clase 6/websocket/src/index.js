b37c40df0a1d8a303468035e003d9dff5b9d440e
Mauricio Gastón <mauuuricio@hotmail.com>
2024-07-27 14:04:50 -0300

    in class 6 b

--- Clase 6/websocket/src/index.js
+++ Clase 6/websocket/src/index.js
@@ -23,12 +23,31 @@ const server = app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
 
-
-const io = new Server(server)
+const io = new Server(server);
 // new Server(server, {cors: transport['websocket']})
 
-const messages = []
+const messages = [];
 
-io.on('connection', (socket)=>{
-    console.log(`Usuario ${socket.id} Connection`)
-})
+io.on("connection", (socket) => {
+  console.log(`Usuario ${socket.id} Connection`);
+  socket.on("userConnect", (data) => {
+    console.log(":::", data);
+    let message = {
+        id: socket.id,
+        info: "connection",
+        name: data.user,
+        message: `usuario: ${data.user} - id: ${data.id} - Conectado`,
+      }
+    messages.push(message);
+
+    io.sockets.emit('userConnect', messages)
+
+  });
+
+  
+
+
+  socket.on("disconnect", () => {
+    console.log("Cliente desconectado:", socket.id);
+  });
+});
--- Clase 6/websocket/src/public/js/index.js
+++ Clase 6/websocket/src/public/js/index.js
@@ -1,10 +1,29 @@
 console.log("IN CLIENT");
 
 const userName = document.querySelector(".userName");
+const chatMessage = document.querySelector(".chatMessage");
 
 const socket = io();
 
-const messages = [];
+var messages = [];
+
+const updateMessagges = (newMessages) => {
+  messages = [...newMessages];
+
+  chatMessage.innerHTML = messages.map((message) => {
+    if (message.info === "connection") {
+      return `<p class="connection">${message.message}</p>`;
+    } else {
+      return `
+        <div class="messageUser">
+          <h5>${message.name}</h5>
+          <p>${message.message}</p>
+        </div>
+      `;
+    }
+  }).join('')
+};
+
 var nameUser = "";
 // Mostrar el formulario de entrada de usuario
 Swal.fire({
@@ -24,4 +43,26 @@ Swal.fire({
     }
     return { name: name, id: id };
   },
+}).then((result) => {
+  console.log("-->", result);
+  const { name, id } = result.value;
+  if (result.isConfirmed) {
+    userName.textContent = name;
+    socket.emit(`userConnect`, { user: name, id });
+  }
 });
+
+socket.on("userConnect", (data) => {
+  console.log(".....", data);
+  chatMessage.innerHTML = ""
+  updateMessagges(data)
+});
+
+const btnMessage = document.getElementById("btnMessage");
+// inputMessage
+
+btnMessage.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("tocando boton")
+})
+
